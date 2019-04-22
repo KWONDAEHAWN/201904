4d0f3faa8a2d5ff3d204a0702df7250a6b337b2a
GD3 <GD3@DESKTOP-KK3U52K>
2019-04-22 18:06:31 +0900

    추가 3

--- src/Test.java
+++ src/Test.java
@@ -12,5 +12,24 @@ public class Test {
 		int num = 100;
 		
 		System.out.println(문자열 + (num + num));
+		
+		System.out.println(1+2+3 + "번");
+		System.out.println("안녕하세요.\n어디서 온 누구시죠?");
+		System.out.println("말티즈\t불독\t시츄");
+		
+		byte b1 = 10;
+		short s1 = 20;
+		int i1 = 100;
+		long l1 = 1000;
+		
+		System.out.println("b1=" + b1);
+		System.out.println("s1=" + s1);
+		System.out.println("i1=" + i1);
+		System.out.println("l1=" + l1);
+		
+		float f = 0.1f;
+		double d = 10.5;
+		System.out.println("f = " + f);
+		System.out.println("d = " + d);
 	}
 }
