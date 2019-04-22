a374c6e285d29d98e63625d156ce4233d2ca83b2
GD3 <GD3@DESKTOP-KK3U52K>
2019-04-22 17:55:21 +0900

    추가 2

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,13 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("HELLO WORLD!");
+		
+		String 문자열 = "답은?";
+		
+		System.out.println(문자열);
+		
+		int num = 100;
+		
+		System.out.println(문자열 + (num + num));
 	}
 }
