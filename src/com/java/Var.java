a1be6ec83da526429b39099dcf64cdc1c0025c27
GD3 <GD3@DESKTOP-KK3U52K>
2019-04-23 12:15:06 +0900

    메소드 연습

---
+++ src/com/java/Var.java
@@ -0,0 +1,29 @@
+package com.java;
+
+public class Var {
+		
+		public Var() {
+			
+			var();
+		}// 기본생성자
+	// 리턴유형 함수명(매계변수) 로직 구현
+	//   void var()        {}	
+		
+		public void  var()	      {
+
+		int a; // 1
+		a = 1; // 2
+		System.out.println(a); // 3
+		
+		int b;
+		b = 2;
+		System.out.println(b);
+		
+		int c,d,e;
+		c = a;
+		d = b;
+		e = c + d;
+		System.out.println(e * 2);
+
+	}
+}
